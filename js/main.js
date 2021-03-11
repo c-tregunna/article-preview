92d9368c5e7eb1af6e8010c529b0842acb0c1bc2
Claire Tregunna <c.tregunna1980@gmail.com>
2021-03-11 21:35:40 +0000

    Add social buttons on click for mobile

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const socialShare = document.querySelector('.article_social_share');
+const socialButtons = document.querySelector('.article_social_share_buttons');
+const socialIcon = document.querySelector('#path');
+const socialIconWrap = document.querySelector('.article_social_share');
+
+let screenWidth = window.innerWidth;
+console.log(screenWidth);
+
+
+socialShare.addEventListener('click', () => {
+    if(screenWidth < 768) {
+        socialButtons.classList.toggle('show');
+        socialIcon.classList.toggle('invert_svg');
+        socialIconWrap.classList.toggle('social_change');
+    }
+});
